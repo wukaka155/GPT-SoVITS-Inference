7f4420c63cf2e38be2f931217eab96be7e6b8939
红血球AE3803 <2544390577@qq.com>
2025-06-05 15:57:16 +0900

    Update gsvi.bat

--- gsvi.bat
+++ gsvi.bat
@@ -2,5 +2,5 @@ set "SCRIPT_DIR=%~dp0"
 set "SCRIPT_DIR=%SCRIPT_DIR:~0,-1%"
 cd /d "%SCRIPT_DIR%"
 set "PATH=%SCRIPT_DIR%\runtime;%PATH%"
-runtime\python.exe gsvi.py -d cuda -p 8000
+runtime\python.exe gsvi.py -p 8000
 pause
